719f66b7429a07a3a5b9e6c2b313ce2210fbc32c
HishamSn <hish.sn@gmail.com>
2017-01-27 00:43:20 +0200

    Creat ShopModel

---
+++ app/src/main/java/al7ag/z1tec/Shop/ShopModel.java
@@ -0,0 +1,21 @@
+package al7ag.z1tec.Shop;
+
+/**
+ * Created by SS on 1/26/2017.
+ */
+
+public class ShopModel
+{
+String ID;
+String Name;
+
+
+String Qty;
+String Mobile;
+String Location;
+//int status;
+
+
+
+
+}
